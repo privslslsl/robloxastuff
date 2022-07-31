a2e589998c31e024f2bcfa0bbda96bd403e3b76a
privslslsl <51520454+privslslsl@users.noreply.github.com>
2022-07-31 04:31:38 -0700

    Update AIM.lua

--- AIM.lua
+++ AIM.lua
@@ -1,65 +1,74 @@
+-- Local Vars
+
+local rs = game:GetService("RunService")
 local UIS = game:GetService("UserInputService")
 local camera = game.Workspace.CurrentCamera
 local p = game:GetService("Players")
 local lp = p.LocalPlayer
 local mouse = lp:GetMouse()
 
+--- AIM Specific Settings
+
 local AIM = {
-	aimKey = "LeftAlt",
+    aimKey = "LeftAlt",
+    aimActivated = false,
 	maxDist = 200,
 	fovMax = 100,
 	fovVis = false,
 	aimState = false,
-	aimSpot = "Head"
+	aimSpot = "Head",
+	teamCheck = false
 }
 
-local fovCircle = Drawing.new("Circle")
-fovCircle.Visible = AIM.fovVis
-fovCircle.Radius = AIM.fovMax
-fovCircle.Position = Vector2.new(camera.ViewportSize.X/2,camera.ViewportSize.Y/2)
-fovCircle.Thickness = 1
+--- Drawing Circle (FOV)
 
-function getPlayer()
-	local cPlayer = nil
-	local cDist = math.huge
-	
-	for i,v in pairs(p:GetPlayers()) do
-		if v ~= lp and v ~= nil then
-		    local p2D, onS = camera:WorldToViewportPoint(v.Character.Head.Position)
-			local mousePos = Vector2.new(mouse.X,mouse.Y)
-			local playerPos = Vector2.new(p2D.X,p2D.Y)
-			local fovDist = (mousePos - playerPos).Magnitude
-			local Dist = (lp.Character.HumanoidRootPart.Position - v.Character.HumanoidRootPart.Position).Magnitude
-			if onS then
-    			if Dist <= AIM.maxDist and fovDist < AIM.fovMax then
-    				cDist = Dist
-    				cPlayer = v
-    			end
-		    end
-		end
-	end
-	return cPlayer
-end
+local fovC = Drawing.new("Circle")
+fovC.Visible = AIM.fovVis
+fovC.Radius = AIM.fovMax
+fovC.Color = Color3.fromRGB(255,255,255)
+fovC.Thickness = 1.15
 
-getgenv().aimCheck = false
+--- Aimbot/Aimkey Control
+
 UIS.InputBegan:Connect(function(inpt)
-    if AIM.aimState == true then
-	if inpt.KeyCode == Enum.KeyCode[AIM.aimKey] then
-		getgenv().aimCheck = true
-		while task.wait() do
-		    if getPlayer() ~= nil and getPlayer().Character.Humanoid.Health > 0 then
-            	camera.CFrame = CFrame.new(camera.CFrame.Position,getPlayer().Character[AIM.aimSpot].Position)
-                if getgenv().aimCheck == false then return end
-            end
-		end
+    if inpt.UserInputType == Enum.UserInputType.MouseButton2 then
+        AIM.aimActivated = true
     end
-end
 end)
 
 UIS.InputEnded:Connect(function(inpt)
-    if inpt.KeyCode == Enum.KeyCode[AIM.aimKey] then
-    	getgenv().aimCheck = false
+    if inpt.UserInputType == Enum.UserInputType.MouseButton2 then
+        AIM.aimActivated = false
     end
 end)
 
-return AIM
+rs.RenderStepped:Connect(function()
+    local cPlayer = nil
+	local cDist = math.huge
+	
+	if AIM.aimActivated then
+	    
+	    for i,v in next, p:GetChildren() do
+	        if v ~= lp and v ~= nil and v.Character:FindFirstChild("Humanoid").Health > 0 then
+	            if AIM.teamCheck == true and v.Team ~= lp.Team or AIM.teamCheck == false then
+	                
+        	        local p2D, onS = camera:WorldToViewportPoint(v.Character.Head.Position)
+        			local mousePos = Vector2.new(mouse.X,mouse.Y)
+        			local playerPos = Vector2.new(p2D.X,p2D.Y)
+        			local fovDist = (mousePos - playerPos).Magnitude
+        			local Dist = (lp.Character.HumanoidRootPart.Position - v.Character.HumanoidRootPart.Position).Magnitude
+        	                
+        	        if onS then 
+        	           if Dist <= AIM.maxDist and fovDist < AIM.fovMax then
+            				cDist = Dist
+            				cPlayer = v
+            			end
+        	        end 
+	            end
+	        end
+	    end
+        if cPlayer ~= nil and cPlayer:FindFirstChild("Humanoid").Health > 0 and AIM.aimState == true then
+            camera.CFrame = CFrame.new(camera.CFrame.Position, getPlayer().Character[AIM.aimSpot].Position)
+        end
+    end
+end)
