e0a65246bb6ec92bb86b067c3d8a52f43931f349
privslslsl <51520454+privslslsl@users.noreply.github.com>
2022-07-31 03:28:48 -0700

    Update AIM.lua

--- AIM.lua
+++ AIM.lua
@@ -4,35 +4,34 @@ local p = game:GetService("Players")
 local lp = p.LocalPlayer
 local mouse = lp:GetMouse()
 
-getgenv().aimToggle = false
-
 local AIM = {
 	aimKey = "LeftAlt",
 	maxDist = 200,
 	fovMax = 100,
-	fovVis = false
+	fovVis = true,
+	aimState = true,
+	aimSpot = "Head"
 }
 
 local fovCircle = Drawing.new("Circle")
 fovCircle.Visible = AIM.fovVis
-fovCircle.Radius = aimSettings.fovMax
+fovCircle.Radius = AIM.fovMax
 fovCircle.Position = Vector2.new(camera.ViewportSize.X/2,camera.ViewportSize.Y/2)
 fovCircle.Thickness = 1
-
 
 function getPlayer()
 	local cPlayer = nil
 	local cDist = math.huge
 	
 	for _,v in pairs(p:GetPlayers()) do
-		if v ~= lp and v.Team ~= lp.Team and v.Character.Humanoid.Health > 0 then
-		    local p2D, onS = camera:WorldToViewportPoint(v.Character.HumanoidRootPart.Position)
+		if v ~= lp and v ~= nil and v.Team ~= lp.Team then
+		    local p2D, onS = camera:WorldToViewportPoint(v.Character.Head.Position)
 			local mousePos = Vector2.new(mouse.X,mouse.Y)
 			local playerPos = Vector2.new(p2D.X,p2D.Y)
 			local fovDist = (mousePos - playerPos).Magnitude
 			local Dist = (lp.Character.HumanoidRootPart.Position - v.Character.HumanoidRootPart.Position).Magnitude
 			if onS then
-    			if Dist <= aimSettings.maxDist and fovDist < aimSettings.fovMax then
+    			if Dist <= AIM.maxDist and fovDist < AIM.fovMax then
     				cDist = Dist
     				cPlayer = v
     			end
@@ -42,30 +41,31 @@ function getPlayer()
 	return cPlayer
 end
 
-function AIM:Toggle(bool)
+function Toggle(bool)
     if bool == true then
-        getgenv().aimToggle = true 
-    else
-        getgenv().aimToggle = false
+        AIM.aimState = true
+    else 
+        AIM.aimState = false
     end
 end
 
 getgenv().aimCheck = false
-
 UIS.InputBegan:Connect(function(i)
-    if getgenv().aimToggle == true then
-	if i.KeyCode == Enum.KeyCode[aimSettings.aimKey] then
+    if AIM.aimState == true then
+	if i.KeyCode == Enum.KeyCode[AIM.aimKey] then
 		getgenv().aimCheck = true
 		while task.wait() do
-    		camera.CFrame = CFrame.new(camera.CFrame.Position,getPlayer().Character.Head.Position)
-    	    if getgenv().aimCheck == false then return end
-    	end
-	end
+		    if getPlayer() ~= nil then
+        	    camera.CFrame = CFrame.new(camera.CFrame.Position,getPlayer().Character[AIM.aimSpot].Position)
+                if getgenv().aimCheck == false then return end
+		    end
+		end
+    end
 end
 end)
 
 UIS.InputEnded:Connect(function(i)
-    if i.KeyCode == Enum.KeyCode[aimSettings.aimKey] then
+    if i.KeyCode == Enum.KeyCode[AIM.aimKey] then
     	getgenv().aimCheck = false
     end
 end)
